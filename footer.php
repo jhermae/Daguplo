867db0c59f8580b05c453618311b92242b3f335e
jhermae <daguplojhermae19@gmail.com>
2024-09-22 22:54:07 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-push-3 footerBlock" style="padding:0px;">
+                <div class="container-fluid" id="about">
+                    <div class="row" style="background-image:url(Img/naturalblack.png)">
+                        <div class="col-md-4 col-xs-2">
+                            <i class="fa fa-twitter aboutIcon"></i>
+                        </div>
+                        <div class="col-md-8 col-xs-10" style="padding-left:30px;padding-bottom:0px;">
+                            <br>
+                            <p>Futa ASPINIANS No.1 Twitter</p>
+                            <p>Tell ASPINIANS:</p>
+                            <p><a href="whatsapp://send?text=Guys... Check Out this new Website: Futatweets.com, My username is: <?php echo $_SESSION['username']?> !" data-action="share/whatsapp/share"><span class="fa fa-whatsapp"></span> Share via Whatsapp</a></p>
+                            
+                            <p> <a href="" target="_blank"><span class="fa fa-facebook"></span> Share via Facebook</a></p>
+                            
+                            <p class="aboutInfo"><span class="glyphicon glyphicon-phone-alt"></span> <b>HelpDesk:</b> <a href="mailto:Gerad17paypa@gmail.com">admins@futatweets.com</a></p>
+
+                            <p>&copy; All Right Reserved@2017 z3r0oo </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
